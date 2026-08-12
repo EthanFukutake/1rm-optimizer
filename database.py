c68b4f471c136f8ca0c589ac8a7d7ac43474ad14
Ethan Fukutake <efukutake23@gmail.com>
2026-08-12 12:35:19 -0500

    Created Database

---
+++ database.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+
+def create_database():
+    connection = sqlite3.connect("workouts.db")
+
+    cursor = connection.cursor()
+
+    create_table = """
+    CREATE TABLE IF NOT EXISTS workout_history (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        weight REAL NOT NULL,
+        reps INTEGER NOT NULL,
+        one_rep_max REAL NOT NULL,
+        date_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+    """
+
+    cursor.execute(create_table)
+    connection.commit()
+
+    connection.close()
+
+
+if __name__ == "__main__":
+    create_database()
